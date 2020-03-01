c3bc1916e289aba4c9cad09c59526c5a2436ec3b
Sarah Condon <s.j.condon01@gmail.com>
2020-03-01 11:58:21 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'signup' => 'users#new'
+  post 'signup' => 'users#create'
+  get 'signin'=> 'sessions#login'
+  post 'signin' => 'sessions#create'
+  delete 'logout' => 'sessions#logout'
+  root 'welcome#index'
+
+  resources :users, only: [:new, :create, :show]
+  resources :attractions, except: [:destroy]
+  resources :rides, only: [:create]
 end
